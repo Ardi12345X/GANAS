34e0735cb8c315050da3ab071e3e78ac1337b025
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-03-21 00:40:51 +0700

    Add files via upload

---
+++ VIRUSDS.sh
@@ -0,0 +1,81 @@
+#author nya jangan di ganti bro 
+#belajar bro supaya bisa buat sc sendiri
+# sv no wa saya +6283192644730
+#script ini hanya untuk hiburan aja
+sleep 5
+echo -e "\e[91mhecked by M3R4Z
+\e[0m";
+sleep 4
+echo -e "\e[95msubscribe chennel MR M3R4Z\e[0m";
+sleep 3
+echo -e "\e[90m}<<<<<<<<<<<<<<VIRUSDS>>>>>>>>>>>>>>{
+\e[0m";
+sleep 5
+echo -e "\e[32m«TCT=Termux•Cyber•Tim»\e[0m";
+sleep 3
+echo -e "\e[32mAuthor = Ardy\e[0m";
+sleep 4
+echo -e "\e[36mno WA =+6283192644730\e[0m";
+sleep 2
+echo -e "\e[95mjangan di salah gunakan!\e[0m";
+sleep 4
+echo -e "\e[91mmasukan no target\e[0m";
+sleep 1
+read -p '}==}>>>>>' userinput1;
+#
+sleep 2
+echo -e "\e[33mtunggu sebentar\e[0m";
+sleep 3
+echo -e "\e[31mloading\e[0m";
+sleep 2
+echo -e "\e[34mloading\e[0m";
+sleep 6
+echo -e "\e[36mloading\e[0m";
+sleep 6
+echo -e "\e[94mloading\e[0m";
+sleep 8
+echo -e "\e[37mmasukan kembali no target\e[0m";
+sleep 6
+read -p '}==}>>>>>' userinput2;
+#
+if [ "${@:-}" = "" ]
+then
+	echo -e "\e[92mtunggu sebentar!\e[0m";
+fi
+sleep 10
+echo -e "\e[31mloading\e[0m";
+sleep 5
+echo -e "\e[93mloading\e[0m";
+sleep 5
+echo -e "\e[92mberhasil mengirim virus10%\e[0m";
+sleep 6
+echo -e "\e[90mberhasil mengirim virus20%\e[0m";
+sleep 3
+echo -e "\e[94mberhasil mengirim virus30%\e[0m";
+sleep 8
+echo -e "\e[35mberhasil mengirim virus40%\e[0m";
+sleep 4
+echo -e "\e[93mberhasil mengirim virus50%\e[0m";
+sleep 9
+echo -e "\e[92mberhasil mengirim virus60%\e[0m";
+sleep 2
+echo -e "\e[34mberhasil mengirim virus70%\e[0m";
+sleep 8
+echo -e "\e[31mberhasil mengirim virus80%\e[0m";
+sleep 4
+echo -e "\e[93mberhasil mengirim virus90%\e[0m";
+sleep 11
+echo -e "\e[94mberhasil mengirim virus100%\e[0m";
+sleep 6
+echo -e "\e[92mVIRUS SUDAH BERHASIL DI KIRIM\e[0m";
+sleep 5
+echo -e "\e[92mjika sc ini tidak berpungsi berarti sc sudah tidak work!\e[0m";
+sleep 5
+echo -e "\e[91mBilly,Devin,Ansiver,006,Gion,Niko,Seven,Reski,Akmal,Anjuna,Taufik,Arya\e[0m";
+sleep 3
+echo -e "\e[32mketik exit untuk keluar!\e[0m";
+sleep 2
+read -p '•' userinput3;
+#
+sleep 14
+#selamat anda kena prenk sc ini tidak work :)
